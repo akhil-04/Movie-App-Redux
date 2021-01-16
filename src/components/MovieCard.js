c8ea6b5798a6af1863b95dd5b52f59681d74ef98
akhil-04 <svam1008@gmail.com>
2021-01-16 15:48:01 +0530

    unfavourite the movie part done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addFavourite } from '../actions';
+import { addFavourite, removeFromFavourites } from '../actions';
 
 class MovieCard extends React.Component {
 
@@ -7,7 +7,10 @@ handleFavouriteClick = ()=>{
     const { movie } = this.props;
     this.props.dispatch(addFavourite(movie));
 }
-handleUnfavouriteClick = ()=>{}
+handleUnfavouriteClick = ()=>{
+    const { movie } = this.props;
+    this.props.dispatch(removeFromFavourites(movie));
+}
 
 render(){
     const {movie, isFavourite} = this.props;
