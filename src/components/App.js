343e06934b0ebde88e368940cf3ffce7dc37ccba
akhil-04 <svam1008@gmail.com>
2021-01-16 15:34:56 +0530

    favourite the movie part done

--- src/components/App.js
+++ src/components/App.js
@@ -18,10 +18,20 @@ class App extends React.Component {
     console.log("STATE", this.props.store.getState());
   }
 
+  isMovieFavourite = (movie)=>{
+    const { favourites } = this.props.store.getState();
+    const index = favourites.indexOf(movie);
+
+    if(index !== -1){
+      //found the movie
+      return true;
+    }
+    return false;
+  }
   render() {
-    console.log("RENDER", this.props.store.getState());
     // const movies = this.props.store.getState(); when it is a array
     const { list } = this.props.store.getState();  //when it becomes object (list, favrouites)
+    console.log("RENDER", this.props.store.getState());
     return (
       <div className="App">
         <Navbar />
@@ -33,7 +43,12 @@ class App extends React.Component {
 
           <div className="list">
             {list.map((movie, index) => (
-              <MovieCard movie={movie} key={`movies-${index}`} />
+              <MovieCard 
+                movie={movie} 
+                key={`movies-${index}`} 
+                dispatch={this.props.store.dispatch}
+                isFavourite = {this.isMovieFavourite(movie)}
+              />
             ))}
           </div>
         </div>
